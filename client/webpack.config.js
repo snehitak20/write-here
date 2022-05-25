847d25f7787b26df8886fe9437aa78f331bd2bf5
Snehita Kolli <snehitakolli20@gmail.com>
2022-05-24 21:33:03 -0700

    minor spelling fix in webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        titlle: 'JATE',
+        title: 'JATE',
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
